3a3e7b06d66c90e118992cfbc1a369f1402a7bef
yutasato21 <yuta0201.t.chan@icloud.com>
2021-12-15 10:56:09 +0900

    テストコード追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,12 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: '全角文字を使用してください'} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: 'には全角文字を使用してください'} do
     validates :first_name
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください'} do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください'} do
     validates :first_name_japanese
     validates :last_name_japanese
   end
