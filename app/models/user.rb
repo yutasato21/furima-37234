a5fade23f7e790d564797395173a091d07a09f7a
yutasato21 <yuta0201.t.chan@icloud.com>
2021-12-15 14:06:09 +0900

    不要バリデーション削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,            presence: true
-  validates :first_name,          presence: true
-  validates :last_name,           presence: true
-  validates :first_name_japanese, presence: true
-  validates :last_name_japanese,  presence: true
   validates :birthday,            presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
