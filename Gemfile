984c2790bd19ad2e2bb5c357a386b8c94d4ff961
yutasato21 <yuta0201.t.chan@icloud.com>
2021-12-15 17:19:08 +0900

    ActiveHashの作成

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ gem 'pry-rails'
 gem 'mini_magick'
 
 gem 'image_processing'
+
+gem 'active_hash'
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,13 +2,20 @@ class Product < ApplicationRecord
   validates :name,               presence: true
   validates :text,               presence: true
   validates :image,              presence: true
-  validates :category_id,        presence: true
-  validates :situation_id,       presence: true
-  validates :delivery_charge_id, presence: true
-  validates :prefectures_id,     presence: true
-  validates :delivery_day_id,    presence: true
+  validates :category_id,        numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation_id,       numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefectures_id,     numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_day_id,    numericality: { other_than: 1 , message: "can't be blank"}
   validates :price,              presence: true
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :delivery_charge
+  belongs_to :prefectures
+  belongs_to :delivery_days
 end
