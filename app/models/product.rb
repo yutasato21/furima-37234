f45408875ff0508324b5c872e02bc9ebacf83387
yutasato21 <yuta0201.t.chan@icloud.com>
2021-12-15 15:14:40 +0900

    Productモデルにバリデーション記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
+  validates :name,               presence: true
+  validates :text,               presence: true
+  validates :image,              presence: true
+  validates :category_id,        presence: true
+  validates :situation_id,       presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefectures_id,     presence: true
+  validates :delivery_day_id,    presence: true
+  validates :price,              presence: true
+
   belongs_to :user
   has_one_attached :image
 end
