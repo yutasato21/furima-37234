ce238bd5b9392d84c62c2d89740a9caaf5c7a4b9
yutasato21 <yuta0201.t.chan@icloud.com>
2021-12-16 17:09:40 +0900

    ログインしていない状態で出品画面に行こうとするとログイン画面に遷移するようapplication_controllerに記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefectures_id,     numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_day_id,    numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,              presence: true, format: { with: /[3-9][0-9]{2}|[1-9][0-9]{3,6}/, message: 'は半角数値300~9,999,999の間で入力'}
+  validates :price,              presence: true, format: { with: /[3-9][0-9]{2}|[1-9][0-9]{3,6}/, message: 'は半角数値のみ300~9999999の間で入力'}
 
   belongs_to :user
   has_one_attached :image
