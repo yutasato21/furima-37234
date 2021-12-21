58e93e456080d163f7e4619ceced046dc1bdca95
yutasato21 <yuta0201.t.chan@icloud.com>
2021-12-21 09:46:41 +0900

    productsコントローラーauthenticate_user!に:edit追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :set_product, only: [:show, :edit, :update]
   before_action :move_to_index, only: :edit
 
