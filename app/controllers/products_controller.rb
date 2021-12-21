103077467cdb9cc8b414571002f01d15d5929f12
yutasato21 <yuta0201.t.chan@icloud.com>
2021-12-21 14:43:15 +0900

    products#destroyアクションコントローラー定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,7 +35,9 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to root_path
   end
 
   private
