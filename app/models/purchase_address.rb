ca20fb96741e6f70a9ff3e315c2052cd457c5908
yutasato21 <yuta0201.t.chan@icloud.com>
2021-12-23 10:59:04 +0900

    Formオブジェクト作成

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,20 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :product_id, :post, :prefectures_id, :municipalities, :number, :building, :telephone, :purchase_id
+
+  with_options presence: true do
+    validates :post, format: {with: /\A[0-9]{3}-[0-9]{4}\Z/, message: "is invalid. Include hyphen(-)"}
+    validates :municipalities
+    validates :number
+    validates :building
+    validates :telephone
+    validates :purchase_id
+  end
+  validates :prefectures_id, numericality: {other_than: 1, message: "can't be blank"}
+
+  def save
+    purchase = Purchase.create(user_id: user_id, product_id: product_id)
+
+    Address.create(post: post, prefectures_id: prefectures_id, municipalities: municipalities, number: number, building: building, telephone: telephone, purchase_id: purchase.id)
+  end
+end
