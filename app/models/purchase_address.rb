2a5d2d2fa0e57e62cd358a61f12c5e94d1b9bacb
yutasato21 <yuta0201.t.chan@icloud.com>
2021-12-24 12:03:41 +0900

    PurchaseAddressモデル不要バリデーション削除

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -9,8 +9,6 @@ class PurchaseAddress
     validates :municipalities
     validates :number
     validates :telephone
-    validates :user_id
-    validates :product_id
     validates :token
   end
   validates :prefectures_id, numericality: {other_than: 1, message: "can't be blank"}
